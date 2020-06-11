b826bc4d4c35cf40ea2012ed50f5309ac2d1e294
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-06-10 20:47:00 -0400

    Contexto

---
+++ PrimerParcialBlazor/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcialBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source: c:\Users\LENOVO\source\repos\ArticulosDb\ArticulosDb.db ");
+        }
+    }
+}
